503606f6ef948a6143d8333cee3d71c49d425c8b
abaduna~ <artuelrey@gmail.com>
2024-05-31 16:54:27 -0300

    feat: eliminar y agregar usarios

---
+++ src/main/java/com/mycompany/sistemadegestiondecliente2/SistemadeGestionDeCliente2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemadegestiondecliente2;
+
+/**
+ *
+ * @author artur
+ */
+public class SistemadeGestionDeCliente2 {
+
+    public static void main(String[] args){
+        Formulario ventana = new Formulario();
+        ventana.show();
+      }
+}
